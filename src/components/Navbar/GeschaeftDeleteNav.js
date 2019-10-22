12e545c9e96b5586d694017f4c08534702486eff
Alexander Gabriel <alex.barbalex@gmail.com>
2019-10-22 08:52:42 +0200

    use svg icon

--- src/components/Navbar/GeschaeftDeleteNav.js
+++ src/components/Navbar/GeschaeftDeleteNav.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { NavItem, Glyphicon } from 'react-bootstrap'
+import { NavItem } from 'react-bootstrap'
 import styled from 'styled-components'
 import { observer, inject } from 'mobx-react'
 import compose from 'recompose/compose'
+import { FaTrashAlt } from 'react-icons/fa'
 
 // eslint-disable-next-line no-unused-vars
 const StyledNavItem = styled(NavItem)`
-  border-right: ${props => (props['data-showgeschaeftenavs'] ? 'solid grey 1px' : 'dotted #505050 1px')};
+  border-right: ${props =>
+    props['data-showgeschaeftenavs'] ? 'solid grey 1px' : 'dotted #505050 1px'};
 `
 
-const enhance = compose(inject('store'), observer)
+const enhance = compose(
+  inject('store'),
+  observer,
+)
 
 const NavbarGeschaeftLoeschenNav = ({ store }) => {
   const { geschaeftSetDeleteIntended, showGeschaefteNavs } = store
@@ -22,7 +27,7 @@ const NavbarGeschaeftLoeschenNav = ({ store }) => {
       disabled={!activeId}
       data-showgeschaeftenavs={showGeschaefteNavs}
     >
-      <Glyphicon glyph="trash" />
+      <FaTrashAlt />
     </StyledNavItem>
   )
 }
