2728efb4a33c01491216adb0182d6f144a1fcafa
Alexander Gabriel <alex.barbalex@gmail.com>
2019-10-23 18:09:15 +0200

    modernize

--- src/components/Table/TableItem.js
+++ src/components/Table/TableItem.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { observer, inject } from 'mobx-react'
-import compose from 'recompose/compose'
-import withHandlers from 'recompose/withHandlers'
+import React, { useContext, useCallback } from 'react'
+import { observer } from 'mobx-react'
 import Linkify from 'react-linkify'
 import _ from 'lodash'
 import $ from 'jquery'
 import styled from 'styled-components'
+
+import storeContext from '../../storeContext'
 
 const StyledRow = styled.div`
   border-bottom: 1px solid #bbbbbb;
@@ -27,35 +26,28 @@ const StyledBodyCell = styled.div`
   max-width: ${props => `${props.maxWidth}px`};
 `
 
-const enhance = compose(
-  inject('store'),
-  withHandlers({
-    onClickTableRow: props => () => {
-      const { store, index } = props
-      const { tableRowToggleActivated } = store
-      const { table, rows } = store.table
-      const row = rows[index]
-      tableRowToggleActivated(table, row.id)
-    },
-  }),
-  observer,
-)
-
-const TableItem = ({ store, index, onClickTableRow }) => {
-  const { rows, id } = store.table
+const TableItem = ({ index }) => {
+  const store = useContext(storeContext)
+  const { tableRowToggleActivated } = store
+  const { table, rows, id } = store.table
   const row = rows[index]
   const { config } = store.app
   const keys = Object.keys(row)
   const values = _.values(row)
   const windowWidth = $(window).width()
-  const tableWidth = windowWidth * config.tableColumnWidth / 100
+  const tableWidth = (windowWidth * config.tableColumnWidth) / 100
   const normalFieldWidth = (tableWidth - 50) / (keys.length - 1)
   const isActive = !!id && id === row.id
+
+  const onClickTableRow = useCallback(
+    () => tableRowToggleActivated(table, row.id),
+    [row.id, table, tableRowToggleActivated],
+  )
 
   return (
     <StyledRow isActive={isActive}>
       {values.map((val, i) => (
-        <StyledBodyCell // eslint-disable-line jsx-a11y/no-static-element-interactions
+        <StyledBodyCell
           key={i}
           maxWidth={keys[i] === 'id' ? 50 : normalFieldWidth}
           onClick={onClickTableRow}
@@ -67,12 +59,4 @@ const TableItem = ({ store, index, onClickTableRow }) => {
   )
 }
 
-TableItem.displayName = 'TableItem'
-
-TableItem.propTypes = {
-  store: PropTypes.object.isRequired,
-  index: PropTypes.number.isRequired,
-  onClickTableRow: PropTypes.func.isRequired,
-}
-
-export default enhance(TableItem)
+export default observer(TableItem)
