6055e6cb1216313af144f96d52de3866d82ea59f
Alexander Gabriel <alex.barbalex@gmail.com>
2019-10-27 17:38:14 +0100

    optimize mst

--- src/store/GeschaefteKontakteExtern.js
+++ src/store/GeschaefteKontakteExtern.js
@@ -1,12 +1,14 @@
 import { types } from 'mobx-state-tree'
 
+import GeschaeftKontaktExtern from './GeschaeftKontaktExtern'
+
 export default types
   .model('GeschaefteKontakteExtern', {
     fetching: types.optional(types.boolean, false),
     activeIdGeschaeft: types.maybeNull(types.number),
     activeIdKontakt: types.maybeNull(types.number),
+    geschaefteKontakteExtern: types.array(GeschaeftKontaktExtern),
   })
   .volatile(() => ({
     error: [],
-    geschaefteKontakteExtern: [],
   }))
--- src/store/GeschaefteKontakteIntern.js
+++ src/store/GeschaefteKontakteIntern.js
@@ -1,16 +1,13 @@
 import { types } from 'mobx-state-tree'
 
-const Gki = types.model('GKI', {
-  idGeschaeft: types.maybeNull(types.number),
-  idKontakt: types.maybeNull(types.number),
-})
+import GeschaeftKontaktIntern from './GeschaeftKontaktIntern'
 
 export default types
   .model('GeschaefteKontakteIntern', {
     fetching: types.optional(types.boolean, false),
     activeIdGeschaeft: types.maybeNull(types.number),
     activeIdKontakt: types.maybeNull(types.number),
-    geschaefteKontakteIntern: types.array(Gki),
+    geschaefteKontakteIntern: types.array(GeschaeftKontaktIntern),
   })
   .volatile(() => ({
     error: [],
