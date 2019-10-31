33a86963df6da900bace2bc0a35738d20df9d3e9
Alexander Gabriel <alex.barbalex@gmail.com>
2019-10-31 15:59:35 +0100

    reduce computed values

--- src/src/addComputedValuesToGeschaefte.js
+++ src/src/addComputedValuesToGeschaefte.js
@@ -4,16 +4,12 @@ import getItKontoForVerantwortlich from './getItKontoForVerantwortlich'
 
 export default store =>
   toJS(store.geschaefte.geschaefte).map(g => {
-    const { geko, links, faelligeStatiOptions } = store.geschaefte
+    const { geko, links } = store.geschaefte
     g.verantwortlichItKonto = getItKontoForVerantwortlich(
       store.geschaefte.interneOptions,
       g.verantwortlich,
     )
     g.geko = geko.filter(gko => gko.idGeschaeft === g.idGeschaeft)
     g.links = links.filter(link => link.idGeschaeft === g.idGeschaeft)
-    g.kannFaelligSein =
-      faelligeStatiOptions && faelligeStatiOptions.includes
-        ? faelligeStatiOptions.includes(g.status)
-        : false
     return g
   })
