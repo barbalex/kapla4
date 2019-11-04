35c35cb9787b9c50897a63d49371e008d63213f3
Alexander Gabriel <alex.barbalex@gmail.com>
2019-11-04 14:21:17 +0100

    optimize

--- src/src/filterGeschaefteByFulltext.js
+++ src/src/filterGeschaefteByFulltext.js
@@ -21,64 +21,11 @@ export default store => {
   } catch (error) {
     console.log('filterGeschaefteByFulltext, error:', error.message)
     addError(error)
+    return geschaefte
   }
+  console.log('filterGeschaefteByFulltext, error 2')
   const filteredIds = result.map(o => o.idGeschaeft)
+  console.log('filterGeschaefteByFulltext, error 3, filteredIds:', filteredIds)
 
-  return geschaefte.filter(g => {
-    /**
-     * this was far too slow
-     * replaced it with fts on db
-     */
-    /*
-    const interne = store.geschaefteKontakteIntern.geschaefteKontakteIntern
-      .filter(k => k.idGeschaeft === geschaeftPassed.idGeschaeft)
-      .map(gk => interneOptions.find(i => i.id === gk.idKontakt) || null)
-    const externe = store.geschaefteKontakteExtern.geschaefteKontakteExtern
-      .filter(k => k.idGeschaeft === geschaeftPassed.idGeschaeft)
-      .map(gk => externeOptions.find(i => i.id === gk.idKontakt) || null)
-
-    // add related data to geschaeft
-    const geschaeft = {
-      ...geschaeftPassed,
-      verantwortlichItKonto:
-        getItKontoForVerantwortlich(
-          interneOptions,
-          geschaeftPassed.verantwortlich,
-        ) || '',
-      geko: geko
-        .filter(gko => gko.idGeschaeft === geschaeftPassed.idGeschaeft)
-        .map(g => g.gekoNr)
-        .join(),
-      interne: interne
-        .map(
-          i =>
-            `${i.name || ''} ${i.vorname || ''}  ${i.kurzzeichen ||
-              ''} ${i.abteilung || ''} ${i.eMail || ''} ${i.telefon || ''}`,
-        )
-        .join(),
-      externe: externe
-        .map(
-          i =>
-            `${i.name || ''} ${i.vorname || ''} ${i.firma || ''} ${i.eMail ||
-              ''} ${i.telefon || ''}`,
-        )
-        .join(),
-      links: links
-        .filter(l => l.idGeschaeft === geschaeftPassed.idGeschaeft)
-        .map(l => l.url)
-        .join(),
-    }
-
-    // if any value satisfies the filter, include the geschaeft
-    let satisfiesFilter = false
-    Object.values(geschaeft).forEach(data => {
-      // ignore empty fields
-      if (!(data || data === 0)) return
-      // data is a primitive value
-      if (primitiveSatisfies({ data, filter })) {
-        satisfiesFilter = true
-      }
-    })*/
-    return filteredIds.includes(g.idGeschaeft)
-  })
+  return geschaefte.filter(g => filteredIds.includes(g.idGeschaeft))
 }
