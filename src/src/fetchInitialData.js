12d93b6eda350b57c60247e981c73cb249a49a4d
Alexander Gabriel <alex.barbalex@gmail.com>
2020-01-09 14:39:33 +0100

    simplify startup

---
+++ src/src/fetchInitialData.js
@@ -0,0 +1,24 @@
+import filterForFaelligeGeschaefte from './filterForFaelligeGeschaefte'
+import fetchUsername from './fetchUsername'
+
+export default store => {
+  store.app.fetchUsername(store)
+  store.faelligeStatiOptionsGet()
+  store.geschaefte.fetchGeko()
+  store.geschaefte.fetchLinks()
+  store.interneOptionsGet()
+  store.externeOptionsGet()
+  store.getGeschaefteKontakteIntern()
+  store.getGeschaefteKontakteExtern()
+  store.rechtsmittelErledigungOptionsGet()
+  store.parlVorstossTypOptionsGet()
+  store.statusOptionsGet()
+  store.geschaeftsartOptionsGet()
+  store.aktenstandortOptionsGet()
+  store.rechtsmittelInstanzOptionsGet()
+  store.abteilungOptionsGet()
+  store.geschaefte.fetch()
+  // set filter to fällige
+  store.geschaefte.filterByFields(filterForFaelligeGeschaefte, 'fällige')
+  store.geschaefte.sortByFields('fristMitarbeiter', 'DESCENDING')
+}
