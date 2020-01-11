ff21767c13b986697cede1a1842f8516ca7a8bb5
Alexander Gabriel <alex.barbalex@gmail.com>
2020-01-11 14:53:54 +0100

    migrate to reactstrap

--- src/components/FilterFields/ComparatorSelector.js
+++ src/components/FilterFields/ComparatorSelector.js
@@ -3,20 +3,19 @@
  * Hooks can only be called inside the body of a function component.
  */
 import React, { useContext } from 'react'
-import { InputGroup, FormControl } from 'react-bootstrap'
+import { Input } from 'reactstrap'
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
 
 import storeContext from '../../storeContext'
 
-const StyledFormControl = styled(FormControl)`
-  width: 55px !important;
+const StyledInput = styled(Input)`
+  max-width: 55px !important;
   margin-right: -1px !important;
   border-radius: 0 !important;
   padding-left: 3px !important;
   padding-right: 0 !important;
   font-weight: 900;
-  font-size: 14px !important;
 `
 
 const ComparatorSelector = ({ name, changeComparator }) => {
@@ -27,21 +26,19 @@ const ComparatorSelector = ({ name, changeComparator }) => {
   const comparatorValue = filterField ? filterField.comparator : ''
 
   return (
-    <InputGroup.Button>
-      <StyledFormControl
-        componentClass="select"
-        onChange={e => changeComparator(e.target.value, name)}
-        value={comparatorValue}
-      >
-        <option value="" />
-        <option value="=">&#8776;</option>
-        <option value="===">=</option>
-        <option value="!==">&#60;&#62;</option>
-        <option value="<">&#60;</option>
-        <option value="<=">&#60;=</option>
-        <option value=">">&#62;</option>
-      </StyledFormControl>
-    </InputGroup.Button>
+    <StyledInput
+      type="select"
+      onChange={e => changeComparator(e.target.value, name)}
+      value={comparatorValue}
+    >
+      <option value="" />
+      <option value="=">&#8776;</option>
+      <option value="===">=</option>
+      <option value="!==">&#60;&#62;</option>
+      <option value="<">&#60;</option>
+      <option value="<=">&#60;=</option>
+      <option value=">">&#62;</option>
+    </StyledInput>
   )
 }
 
