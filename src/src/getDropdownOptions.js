2279546aa255d5d7bde2b248f232ab262ce74b69
Alexander Gabriel <alex.barbalex@gmail.com>
2020-07-16 11:24:07 +0200

    comment

--- src/src/getDropdownOptions.js
+++ src/src/getDropdownOptions.js
@@ -2,11 +2,12 @@ export default (store, name) => {
   let result = []
   try {
     result = store.app.db
+      // TODO: update when removing historisch field
       .prepare(`SELECT ${name} FROM ${name} WHERE historisch = 0 ORDER BY sort`)
       .all()
   } catch (error) {
     store.addErrorMessage(error.message)
   }
-  const options = result.map(res => res[name])
+  const options = result.map((res) => res[name])
   return options
 }
