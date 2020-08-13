3aec9d84c330503b7b10e945cdaaa7107f0662a7
Alexander Gabriel <alex.barbalex@gmail.com>
2020-08-13 16:51:59 +0200

    up version

--- src/components/Navbar/More.js
+++ src/components/Navbar/More.js
@@ -80,7 +80,7 @@ const OptionsNav = () => {
           Fehler und Wünsche melden
         </DropdownItem>
         <DropdownItem divider />
-        <Version>Version: 2.1.1 vom 13.8.2020</Version>
+        <Version>Version: 2.1.2 vom 13.8.2020</Version>
       </DropdownMenu>
     </MoreMenu>
   )
