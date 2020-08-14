fc59525c6a4717eb6991f26a58492b7f85bd6aaf
Alexander Gabriel <alex.barbalex@gmail.com>
2020-08-14 15:33:23 +0200

    optimize

--- src/src/getDb.js
+++ src/src/getDb.js
@@ -2,16 +2,22 @@ import Database from 'better-sqlite3'
 
 import chooseDb from './chooseDb'
 import getConfig from './getConfig'
-import saveConfig from './saveConfig'
 import standardDbPath from './standardDbPath'
 
 export default async (store) => {
-  const {addErrorMessage} = store
-  const {setDbPath, setTableColumnWidth, setGeschaefteColumnWidth, setLastWindowState, saveConfig} = store.app
+  const { addErrorMessage } = store
+  const {
+    setDbPath,
+    setTableColumnWidth,
+    setGeschaefteColumnWidth,
+    setLastWindowState,
+    saveConfig,
+  } = store.app
   const config = getConfig()
   if (config.dbPath) setDbPath(config.dbPath)
   if (config.tableColumnWidth) setTableColumnWidth(config.tableColumnWidth)
-  if (config.geschaefteColumnWidth) setGeschaefteColumnWidth(config.geschaefteColumnWidth)
+  if (config.geschaefteColumnWidth)
+    setGeschaefteColumnWidth(config.geschaefteColumnWidth)
   if (config.lastWindowState) setLastWindowState(config.lastWindowState)
   let dbPath = config.dbPath || standardDbPath
 
@@ -27,14 +33,14 @@ export default async (store) => {
       try {
         dbPath = await chooseDb()
       } catch (chooseError) {
-        store.addErrorMessage(chooseError.message)
+        addErrorMessage(chooseError.message)
         return console.log('Error after choosing db:', chooseError)
       }
       setDbPath(dbPath)
       saveConfig({ dbPath })
       db = new Database(dbPath, { fileMustExist: true })
     } else {
-      store.addErrorMessage(error.message)
+      addErrorMessage(error.message)
       return console.log('index.js, Error opening db file:', error)
     }
   }
