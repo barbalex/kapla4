3f3c94584a43ffa6905c999c1459e43a82ca9355
Alexander Gabriel <alex.barbalex@gmail.com>
2020-09-18 18:17:47 +0200

    prevent undefined

--- src/store/Geschaeft.js
+++ src/store/Geschaeft.js
@@ -8,109 +8,118 @@ import convertDateToDdMmYyyy from '../src/convertDateToDdMmYyyy'
 export default types
   .model('Geschaeft', {
     abteilung: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     aktennummer: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     aktenstandort: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     ausloeser: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     datumAusgangAwel: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     datumEingangAwel: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
-    details: types.maybe(types.union(types.string, types.integer, types.null)),
+    details: types.maybe(
+      types.union(types.string, types.integer, types.null, types.undefined),
+    ),
     entscheidAwel: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     entscheidBdv: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     entscheidBvv: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     entscheidKr: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     entscheidRrb: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     fristAbteilung: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     fristAmtschef: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     fristAwel: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     fristDirektion: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     fristMitarbeiter: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     gegenstand: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     geschaeftsart: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
-    idGeschaeft: types.integer,
+    // need maybe when bericht can have no data
+    idGeschaeft: types.maybe(types.integer),
     // there seem to exist '' values in idVorgeschaeft...
     idVorgeschaeft: types.maybe(
-      types.union(types.integer, types.string, types.null),
+      types.union(types.integer, types.string, types.null, types.undefined),
     ),
     mutationsdatum: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     mutationsperson: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     naechsterSchritt: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
-    ort: types.maybe(types.union(types.string, types.integer, types.null)),
+    ort: types.maybe(
+      types.union(types.string, types.integer, types.null, types.undefined),
+    ),
     parlVorstossStufe: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     parlVorstossTyp: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     parlVorstossZustaendigkeitAwel: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     rechtsmittelInstanz: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     rechtsmittelErledigung: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     rechtsmittelEntscheidNr: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     rechtsmittelEntscheidDatum: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     rechtsmittelTxt: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
-    status: types.maybe(types.union(types.string, types.integer, types.null)),
+    status: types.maybe(
+      types.union(types.string, types.integer, types.null, types.undefined),
+    ),
     verantwortlich: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
-    vermerk: types.maybe(types.union(types.string, types.integer, types.null)),
+    vermerk: types.maybe(
+      types.union(types.string, types.integer, types.null, types.undefined),
+    ),
     vermerkIntern: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
     zustaendigeDirektion: types.maybe(
-      types.union(types.string, types.integer, types.null),
+      types.union(types.string, types.integer, types.null, types.undefined),
     ),
   })
   .actions((self) => ({
