c8ecfb06721e8f51ef7391f3a8e8cb47d28e2cda
Alexander Gabriel <alex.barbalex@gmail.com>
2021-08-19 16:02:57 +0200

    lint

--- src/src/fetchUsername.js
+++ src/src/fetchUsername.js
@@ -1,11 +1,16 @@
 import getUsername from 'username'
+// WARNING
+// v6.0.0 imports using esm
+// this seems to break asset-relocator from the build process...
 
-export default store => {
+const fetchUsername = (store) => {
   const { setUsername } = store.app
   const username = getUsername.sync()
   if (username) {
     setUsername(username)
   } else {
-    addErrorMessage('keinen Benutzernamen erhalten')
+    store.addErrorMessage('keinen Benutzernamen erhalten')
   }
 }
+
+export default fetchUsername
